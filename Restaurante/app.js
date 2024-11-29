50cdf34d95c3069cd1482895815947b56bdf4ea3
rmaatzak <159541913+rmaatzak@users.noreply.github.com>
2024-11-29 16:54:48 -0300

    .
    .

---
+++ Restaurante/app.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+  
